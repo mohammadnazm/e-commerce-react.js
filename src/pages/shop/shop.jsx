948924c2538edf863d95e4e29151df9cf63462dd
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-26 23:46:03 +0300

    upgrade

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 
-export const shop = () => {
-  return <div>shop</div>
+const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
+      <div className="Shop"></div>
+    </div>
+  )
 }
+
+export default Shop
