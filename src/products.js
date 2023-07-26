58dd003dd824680a5f7a9cf21b45a4110ea92873
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-26 23:44:21 +0300

    Create products.js

---
+++ src/products.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const products = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  )
+}
+
+export default products
