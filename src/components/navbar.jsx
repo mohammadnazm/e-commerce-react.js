169c077ce8ae579f46ced9dd794f1b9cbe636b48
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-26 21:47:57 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,8 @@ import { ShoppingCart } from "phosphor-react"
 import React from "react"
 import { Link } from "react-router-dom"
 
+import "./navbar.css"
+
 export const Navbar = () => {
   return (
     <div className="navbar">
