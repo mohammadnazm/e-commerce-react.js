ca5ae579510ffd1ca05298c31e37eb5575664123
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-26 21:36:06 +0300

    upgrade

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import { ShoppingCart } from "phosphor-react"
+import React from "react"
+import { Link } from "react-router-dom"
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart />
+        </Link>
+      </div>
+    </div>
+  )
+}
