91c99ef3ae11eb0a1263945e9c0abe7b5322978e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-27 18:26:08 +0300

    Update shop-context.jsx

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,6 +1,15 @@
 import React, { createContext, useState } from "react"
+import { PRODUCTS } from "../products"
 
 export const ShopContext = createContext(null)
+
+const getDefaultCart = () => {
+  let cart = {}
+  for (let i = 1; i < PRODUCTS.length; i++) {
+    cart[i] = 0
+  }
+  return cart
+}
 
 export const ShopContextProvider = props => {
   const [cartItems, setCartItems] = useState()
