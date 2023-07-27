f6b43c19495147e0c6a3ceeb0a7271e9330db068
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-27 17:50:30 +0300

    Create shop-context.jsx

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,8 @@
+import React, { createContext, useState } from "react"
+
+export const ShopContext = createContext(null)
+
+export const ShopContextProvider = props => {
+  const [cartItems, setCartItems] = useState()
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>
+}
