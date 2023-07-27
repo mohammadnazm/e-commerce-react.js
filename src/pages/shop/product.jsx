7f15fc8f5f7fec1bbb00f9f67d86c9912485833e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-27 19:11:20 +0300

    Update product.jsx

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react"
+import { ShopContext } from "../../context/shop-context"
 
 export const Product = props => {
   const { id, productName, price, productImage } = props.data
-
+  const { addToCart } = useContext(ShopContext)
   return (
     <div className="product">
       <img src={productImage} />
