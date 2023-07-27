1e4755c2491764cc91d741bd594f732c3cae081f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-27 11:34:17 +0300

    upgrade

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,6 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 
-export default function Product(props) {
+export const Product = props => {
   const { id, productName, price, productImage } = props.data
-  return <div>{productName}</div>
+
+  return (
+    <div className="product">
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+      </div>
+    </div>
+  )
 }
